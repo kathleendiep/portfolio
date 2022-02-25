6daef721b204564bcc9940e07b4f0f164b5e7b14
Kathleen Diep <kathleendiep@Kathleens-MBP.attlocal.net>
2022-02-25 09:43:22 -0800

    layout added

---
+++ app.js
@@ -0,0 +1,40 @@
+
+
+console.log("LINKED");
+
+$(document).ready(function(){
+    console.log('ready')
+
+    document.getElementById('menu-1').addEventListener('click', function(){
+    document.getElementById('big-menu').classList.add('toggle-open');
+    console.log("LINKED");
+
+    })
+
+    document.getElementById('big-menu-close').addEventListener('click', function(){
+    document.getElementById('big-menu').classList.remove('toggle-open');
+    })
+
+    document.getElementById('project-button-1').addEventListener('click', function(){
+        document.getElementById('project-menu-details').classList.add('toggle-open');
+    })
+ //function   
+    const openDetails = () => {
+        document.getElementById('personal-website').classList.add('toggle-open');
+        $( `#personal-website` ).appendTo$(`#project-button-1`);
+    }
+    const hoverDetails = () => {
+
+        $( `.project-item` ).addClass("hover-project");
+
+    }
+ 
+
+
+$(()=>{
+
+    $('#project-button-1').on('click', openDetails);
+    $('#project-button-1').on('hover', hoverDetails);
+  });
+
+})
