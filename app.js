db1ae227e940b96cbfc15dcba65a93982fe1c101
Kathleen Diep <kathleendiep@Kathleens-MBP.attlocal.net>
2022-02-28 19:23:44 -0800

    added layout of projects

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ console.log("LINKED");
 
 $(document).ready(function(){
     console.log('ready')
+    //HEADER AND FOOTER
     $('header').load('header.html', null, () => {
         document.getElementById('menu-1').addEventListener('click', function(){
             document.getElementById('big-menu').classList.add('toggle-open');
@@ -16,43 +17,40 @@ $(document).ready(function(){
     });
     $('footer').load('footer.html');
 
+    // document.getElementById('project-button-1').addEventListener('click', function(){
+    //     document.getElementById('project-menu-details').classList.add('toggle-open');
+    // })
 
-
-    document.getElementById('project-button-1').addEventListener('click', function(){
-        document.getElementById('project-menu-details').classList.add('toggle-open');
-    })
-
-    
-  //hover over
-    const hoverOver= () => {
+// for button1 - front end 
+    //hover over
+    const hoverOver1= () => {
         $( `.front-end` ).addClass('addDetails');
-        $( `.front-end` ).appendTo$(`#skills-button`);
-      
+        $( `.front-end`).fadeIn(500);
     }
-//hover out
-    const hoverOut= () => {
-        $( this ).find( ".front-end" ).remove();
-
+    //hover out
+    const hoverOut1= () => {
+        $( this ).find( ".front-end" ).fadeOut( "slow");
     }
-
+// for button2 - back end
+    //hover over
+    const hoverOver2= () => {
+        $( `.back-end` ).addClass('addDetails');
+        $( `.back-end`).fadeIn(500);
+    }
+    //hover out
+    const hoverOut2= () => {
+        $( this ).find( `.back-end`).fadeOut( "slow");
+    }
  //function   
     const openDetails = () => {
         document.getElementById('personal-website').classList.add('toggle-open');
         $( `#personal-website` ).appendTo$(`#project-button-1`);
     }
 
-    const hoverDetails = () => {
-
-        $( `.project-item` ).addClass("hover-project");
-
-    }
-
-
-
 $(()=>{
     $('#project-button-1').on('click', openDetails);
-    $('#project-button-1').on('hover', hoverDetails);
-    $( `#skills-button` ).hover(hoverOver,hoverOut);
+    $( `#skills-button1` ).hover(hoverOver1,hoverOut1); 
+    $( `#skills-button2` ).hover(hoverOver2,hoverOut2); 
   });
 
 })
