de2416e2358a84c63de17e4819679f5f2ce72446
Kathleen Diep <kathleendiep@Kathleens-MBP.attlocal.net>
2022-02-28 16:49:45 -0800

    make sure to link weblinks

--- app.js
+++ app.js
@@ -4,17 +4,18 @@ console.log("LINKED");
 
 $(document).ready(function(){
     console.log('ready')
-    $('header').load('header.html');
+    $('header').load('header.html', null, () => {
+        document.getElementById('menu-1').addEventListener('click', function(){
+            document.getElementById('big-menu').classList.add('toggle-open');
+            console.log("LINKED");
+            })
+        
+            document.getElementById('big-menu-close').addEventListener('click', function(){
+            document.getElementById('big-menu').classList.remove('toggle-open');
+            })
+    });
     $('footer').load('footer.html');
 
-    document.getElementById('menu-1').addEventListener('click', function(){
-    document.getElementById('big-menu').classList.add('toggle-open');
-    console.log("LINKED");
-    })
-
-    document.getElementById('big-menu-close').addEventListener('click', function(){
-    document.getElementById('big-menu').classList.remove('toggle-open');
-    })
 
     document.getElementById('project-button-1').addEventListener('click', function(){
         document.getElementById('project-menu-details').classList.add('toggle-open');
